84ba412d809ad8e70d1b9858051ee4c63bdfa26d
Roberto <robertoabad93@gmail.com>
2020-10-27 08:53:21 +0000

    Texto 2 creamos un nuevo contructor con los parametros variables

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -23,7 +23,18 @@ public class MaquinaExpendedoraSimple {
         estacionOrigen = "Leon";
         estacionDestino = "Oviedo";
     }
-
+    /**
+     * Crea una maquina expendedora de billetes de tren con el 
+     * precio del billete y el origen y destino dados. Se asume que el precio
+     * del billete que se recibe es mayor que 0.
+     */
+    public MaquinaExpendedoraSimple(int precioDelBillete,String destino) {
+        precioBillete = precioDelBillete;
+        balanceClienteActual = 0;
+        totalDineroAcumulado = 0;
+        estacionOrigen = "Leon";
+        estacionDestino = destino;
+    }
     /**
      * Devuelve el precio del billete
      */
